0dcc9fad7c96b7c96214584fce2f894edcfe06fe
Manuel <manuelquiroz4254@gmail.com>
2023-02-25 23:03:44 -0300

    Commit inicial

---
+++ Matriz2.java
@@ -0,0 +1,18 @@
+public class Matriz2 {
+
+	public static void main(String[] args) {
+
+            int [][] Matriz = new int [4][4];
+            int i=0;
+            for(int j = 0; j < 4; j++) {
+            	System.out.print(j+1+ ": ");
+            for (int p=0; p<4; p++) {
+            	Matriz [j][p]=i;
+            	i=i+2;
+           System.out.print(" "+ i ); 		
+            }
+            System.out.println(".");
+            System.out.println("Thanks");
+           }
+}
+}
